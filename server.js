d654f0b448a217b5aadd467aa50acfdce02894c6
Ben <majertomas1@seznam.cz>
2026-08-22 13:35:52 +0200

    Fix secure cookie setting for production HTTPS on Railway

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use(session({
     cookie: { 
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
-        secure: false,
+        secure:isProduction,
         sameSite: isProduction ? 'strict' : 'lax'
     }
 }));
